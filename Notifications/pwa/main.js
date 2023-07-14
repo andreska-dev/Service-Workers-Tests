28a07065a1d947a0371148e7c49ef9b1a2ed4125
Andrés Méndez <andres.mendez.solano@est.una.ac.cr>
2023-07-14 11:48:38 -0600

    Notification with permissions and options

--- Notifications/pwa/main.js
+++ Notifications/pwa/main.js
@@ -6,3 +6,34 @@
   // navigator.serviceWorker.register('/sw.js').then(function(registration){
   // }).catch(console.log);
 // }
+
+
+//verificar que sea posible enviar notificaciones
+if(window.Notification){
+
+function showNotification(){
+  let notificationOpts ={
+    body: 'notification information',
+    icon: '/thumb.png'
+  }
+  //incluir las opts
+  let n = new Notification('Nueva Notificacion de PWA.',notificationOpts);
+
+  //listenner del click
+  n.onclick =()=>{
+  console.log('Notification clicked');
+  }
+}
+
+  //Administrar permisos de usuario 
+  if(Notification.permission ==='granted'){
+    showNotification();
+  }else if(Notification.permission !== 'denied'){
+    Notification.requestPermission((permission)=>{
+        if(permission ==='granted'){
+          showNotification();
+        }
+    });
+  }
+
+}
