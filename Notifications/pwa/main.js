cd39ca93d1b8db85784204c573109a04f5f2aaba
Andrés Méndez <andres.mendez.solano@est.una.ac.cr>
2023-07-14 11:22:58 -0600

    NOtifications and REACT

---
+++ Notifications/pwa/main.js
@@ -0,0 +1,8 @@
+
+// Progressive Enhancement (SW supported)
+// if ('serviceWorker' in navigator) {
+// if (navigator.serviceWorker) {
+  // Register the SW
+  // navigator.serviceWorker.register('/sw.js').then(function(registration){
+  // }).catch(console.log);
+// }
