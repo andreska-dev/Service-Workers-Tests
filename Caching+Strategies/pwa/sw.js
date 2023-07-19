bbb4e8e74bd78e30cc2f577eeb037cf3895d1bb6
Andrés Méndez <andres.mendez.solano@est.una.ac.cr>
2023-07-19 13:34:44 -0600

    cache strategies

---
+++ Caching+Strategies/pwa/sw.js
@@ -0,0 +1,66 @@
+// Service Worker
+
+// Cache name
+const pwaCache = 'pwa-cache-1';
+
+// Static assets to cache on install
+const staticCache = [ '/', 'index.html', '/style.css', '/main.js', '/thumb.png' ];
+
+// SW install and cache static assets
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+      caches.open(pwaCache)
+        .then( cache => cache.addAll(staticCache) )
+    );
+});
+
+// SW Activate and cache cleanup
+self.addEventListener('activate', (e) => {
+    let cacheCleaned = caches.keys().then((keys) => {
+        keys.forEach( (key) => {
+            if (key !== pwaCache) return caches.delete(key);
+        });
+    });
+    e.waitUntil(cacheCleaned);
+});
+
+
+// SW fetch handler with different caching strategies
+self.addEventListener('fetch', (e) => {
+    //1. Cache only strategy (static assets) AKA App Shell
+    /*e.respondWith( 
+        caches.match(e.request) 
+    );*/
+    //Si el usuario o el navegador borra cache, no se verá la app
+
+
+
+    //2. cache with network fallback strategy (dynamic content)
+    /*e.respondWith(
+        caches.match(e.request).then( (cacheRes) => {
+            //si esta en cache, la devuelve
+            if(cacheRes) return cacheRes;
+            //si no esta en cache, la pide a la red
+            return fetch(e.request).then( (newRes) => {
+                //y la guarda en cache
+                caches.open(pwaCache).then( (cache) => cache.put(e.request, newRes) );
+                //y la devuelve
+                return newRes.clone();
+            });
+        } )
+    );*/
+    //la estrategia 2 es mas robusta para los static assets
+
+
+    //3. Network with cache fallback strategy (dynamic content)
+    //Asegura que el usuario siempre tenga la ultima version de los datos
+    e.respondWith(
+        fetch(e.request).then( (res) => {
+            //guarda la respuesta en cache
+            caches.open(pwaCache).then( (cache) => cache.put(e.request, res) );
+            //y la devuelve
+            return res.clone();
+            //fallback a cache si no hay conexion
+        }).catch( (err) => caches.match(e.request) )
+    );
+});
