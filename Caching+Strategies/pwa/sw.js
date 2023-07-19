191e46e1c9dd388109f4aaeddd35103863685a9d
Andrés Méndez <andres.mendez.solano@est.una.ac.cr>
2023-07-19 14:01:27 -0600

    More strategies

--- Caching+Strategies/pwa/sw.js
+++ Caching+Strategies/pwa/sw.js
@@ -54,7 +54,9 @@ self.addEventListener('fetch', (e) => {
 
     //3. Network with cache fallback strategy (dynamic content)
     //Asegura que el usuario siempre tenga la ultima version de los datos
-    e.respondWith(
+    //al hacer fetch primero puede que se comporte de manera lenta
+    //generando una mala expereincia de usuario
+   /* e.respondWith(
         fetch(e.request).then( (res) => {
             //guarda la respuesta en cache
             caches.open(pwaCache).then( (cache) => cache.put(e.request, res) );
@@ -62,5 +64,53 @@ self.addEventListener('fetch', (e) => {
             return res.clone();
             //fallback a cache si no hay conexion
         }).catch( (err) => caches.match(e.request) )
-    );
+    );*/
+
+
+    //4. Cache with network update strategy (dynamic content)
+
+    /*e.respondWith(
+        //ir al cache y devolver al usuario, despues hacer el update del cache
+        //la version actualizada siempre va a estar en cache y no mostrandose al usuario
+        caches.open(pwaCache).then( (cache) => {
+            //return from cache
+            return cache.match(e.request).then( (cacheRes) => {
+                //update cache
+                let updatedCache = fetch(e.request).then( (newRes) => {
+                    //si la respuesta es 200, la guarda en cache
+                    cache.put(e.request, newRes.clone());
+                    return newRes; 
+                });
+                //return cache response or updated response
+                return cacheRes || updatedCache;
+            } );
+        } )
+    );*/
+    //funciona pero puede que los datos no esten actualizados, pero es mas rapido
+
+
+
+    //5. cache & network race 
+    //devuelve la respuesta mas rapida, ya sea del cache o de la red
+    let firstResponded = new Promise( (resolve, reject) => {
+
+        //track rejections
+        let firstRejectionReceived = false;
+        let rejectOnce=() =>{
+            if(firstRejectionReceived) reject('No response received');
+            else firstRejectionReceived = true;
+        };
+
+        //try network
+        fetch(e.request).then( (res) => {
+            res.ok ? resolve(res) : rejectOnce();
+        }).catch(rejectOnce);
+
+        //try cache
+        caches.match(e.request).then( (res) => {
+            res ? resolve(res) : rejectOnce();
+        }).catch(rejectOnce);
+    });
+    e.respondWith(firstResponded);
+
 });
