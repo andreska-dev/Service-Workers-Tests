27ecf4afe9e74dfa97a820a44adf0a27949facca
Andrés Méndez <andres.mendez.solano@est.una.ac.cr>
2023-07-27 14:12:15 -0600

    final, deletes subscriptions goned

--- push_server/server/push.js
+++ push_server/server/push.js
@@ -28,9 +28,27 @@ module.exports.addSubscription=(subscription)=>{
 
 //send notifications to all registered subscriptions
 module.exports.send= (message) =>{
+    //notification promises
+    let notifications =[]
     //loop subscritions
     subscriptions.forEach((subscription,i)=>{
         //send notification
-        webpush.sendNotification(subscription,message)
+       let p= webpush.sendNotification(subscription,message)
+            .catch(status=>{
+                //check for "410 gone"
+                if(status.statusCode === 410)
+                    subscriptions[i]['delete']=true;
+                return null
+            })
+    //push notification promise to an array
+        notifications.push(p);
+    })
+
+    //clean subscriptions marked for deletion
+    Promise.all(notifications).then(()=>{
+        //filter subscriptions array
+        subscriptions=subscriptions.filter(subscription => !subscription.delete)
+        //persist 'cleaned' subscriptions
+        store.put('subscriptions',subscriptions)
     })
 }
