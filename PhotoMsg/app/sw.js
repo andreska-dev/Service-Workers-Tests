e442d32456a27a493545a51d8b5cd0295d268da1
Andrés Méndez <andres.mendez.solano@est.una.ac.cr>
2023-09-04 08:03:29 -0600

    sw taking over inmediately

--- PhotoMsg/app/sw.js
+++ PhotoMsg/app/sw.js
@@ -1,6 +1,7 @@
 
 
 //import google workbox
-importScripts(
-    'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
-);
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
+//Allow new SW to take over
+workbox.skipWaiting();
+workbox.clientsClaim();
